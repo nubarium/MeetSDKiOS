a207186ab4819374e347f846c893e6aa9c96d89e
nubarium <roberto.ramos@nubarium.com>
2021-10-27 10:08:14 -0500

    Name Change

---
+++ iOSMeetSDK.framework/Headers/iOSMeetSDK.h
@@ -0,0 +1,18 @@
+//
+//  iOSMeetSDK.h
+//  iOSMeetSDK
+//
+//  Created by Roberto Ramos on 27/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSMeetSDK.
+FOUNDATION_EXPORT double iOSMeetSDKVersionNumber;
+
+//! Project version string for iOSMeetSDK.
+FOUNDATION_EXPORT const unsigned char iOSMeetSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSMeetSDK/PublicHeader.h>
+
+
